9f17be97d8c9831b894e50a9001a39cff0e54e5d
Stephanie Provence <stephanie@lewiscommunications.com>
2019-02-24 18:46:05 -0600

    remove default react-app stuff, update App to functional component and create card

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card from './components/Card';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const mockdata = {
+  name: 'Clark',
+  items: [
+    'Bad Boys 2 Box Set', 
+    'Aluminum Ingot'
+  ]
+}
+
+const App = () => {
+  
+  return (
+    <div>
+      <Card {...mockdata} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Card/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  box-shadow: -2px -2px 7px rgba(0, 0, 0, 0.12), 2px 4px 6px rgba(0, 0, 0, 0.2); 
+  border-radius: 8px;
+  padding: 13px 25px;
+`;
+
+const Name = styled.h2`
+  font-size: 24px;
+`;
+
+const List = styled.ul`
+  margin-top: 0;
+`;
+
+const ListItem = styled.li`
+  list-style: none;
+`;
+
+const Card = ({items = [], name}) => (
+  <Wrapper>
+    <Name>{name}</Name>
+    <List>
+      {items.map(item => (
+        <ListItem>{item}</ListItem>
+      ))}
+    </List>
+  </Wrapper>
+)
+
+export default Card;
